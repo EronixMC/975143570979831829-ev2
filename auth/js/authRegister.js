bd4652d1c7a926f4a5c77a2976e2af976ff1bc07
EronixMC <107801782+EronixMC@users.noreply.github.com>
2022-12-24 00:38:50 +0100

    Add files via upload

---
+++ auth/js/authRegister.js
@@ -0,0 +1,44 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBexyNPogru8Jc7sq4FoZBrFEckPrmBS8c",
+    authDomain: "eronixmc.firebaseapp.com",
+    databaseURL: "https://eronixmc-default-rtdb.firebaseio.com",
+    projectId: "eronixmc",
+    storageBucket: "eronixmc.appspot.com",
+    messagingSenderId: "469680302705",
+    appId: "1:469680302705:web:b2475a19ba090001e109d0",
+    measurementId: "G-6Z685TD6MN"
+};
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+    }
+})
+
+async function register() {
+    const db = firebase.database();
+    const username = document.getElementById("pseudo").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    let userId, user;
+    try {
+        let userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password)
+        if (!userCredential?.user?.uid) {
+            throw new Error("There is no UID!", userCredential);
+        }
+        user = userCredential.user;
+        userId = userCredential.user.uid;
+        console.log(`UID is: ${userId}`);
+
+        await db.ref("users/" + userId).set({
+            username: username,
+            password: password,
+            uid: userId,
+            email: email,
+        });
+        window.location = "./index.html"
+    } catch (ex) {
+        console.error(ex);
+        document.getElementById("error").innerHTML = ex.message
+    }
+}
